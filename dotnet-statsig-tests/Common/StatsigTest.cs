dcac13b667d07edb1fcb99a0053ab70250746bbe
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2023-03-28 21:16:47 -0700

    bump version -> 1.16.0 (#82)

--- dotnet-statsig-tests/Common/StatsigTest.cs
+++ dotnet-statsig-tests/Common/StatsigTest.cs
@@ -21,7 +21,7 @@ namespace dotnet_statsig_tests
     {
         WireMockServer _server;
 
-        private const String ExpectedSdkVersion = "1.15.0.0";
+        private const String ExpectedSdkVersion = "1.16.0.0";
 
         Task IAsyncLifetime.InitializeAsync()
         {
