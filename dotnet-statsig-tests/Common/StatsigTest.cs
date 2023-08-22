1fb8c5364237ac4beb6c552b1c58c28850e2f818
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2023-08-22 12:22:14 -0700

    [release] v1.19.0 - Expose GroupName on DynamicConfig (#108)
    * Bot: Updated File/s [dotnet-statsig/dotnet-statsig.csproj, dotnet-statsig-tests/Common/StatsigTest.cs]
    
    * Update StatsigTest.cs

--- dotnet-statsig-tests/Common/StatsigTest.cs
+++ dotnet-statsig-tests/Common/StatsigTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -21,7 +21,7 @@ namespace dotnet_statsig_tests
     {
         WireMockServer _server;
 
-        private const String ExpectedSdkVersion = "1.18.0.0";
+        private const String ExpectedSdkVersion = "1.19.0.0";
 
         Task IAsyncLifetime.InitializeAsync()
         {
