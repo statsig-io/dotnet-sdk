c5e97097c45787ed78299b474d957aeb2131ab09
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2023-09-12 09:52:38 -0700

    Bot: Updated File/s [dotnet-statsig/dotnet-statsig.csproj, dotnet-statsig-tests/Common/StatsigTest.cs] (#112)

--- dotnet-statsig-tests/Common/StatsigTest.cs
+++ dotnet-statsig-tests/Common/StatsigTest.cs
@@ -21,7 +21,7 @@ namespace dotnet_statsig_tests
     {
         WireMockServer _server;
 
-        private const String ExpectedSdkVersion = "1.19.0.0";
+        private const String ExpectedSdkVersion = "1.20.0.0";
 
         Task IAsyncLifetime.InitializeAsync()
         {
