890e6d656110c89dc66a03008d99473b705b5d6f
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2024-08-06 14:52:34 -0700

    [release] 1.27.3 - Removed Private Attributes and Hashed Secondary Exposures in `GCIR` (#157)
    ### Fixes
    - Removed the `private attributes` on user object in
    `getClientInitializeReponse`
     - Hashed secondary exposures in `getClientInitializeReponse`
     
    
    
    
    
    
    >Included In This Release
    >- 3a2936dfba68796534e782780f701e5474f10058 Weihao Ding
    > - fix: remove private attributes from user object when bootstrap
    client sdk (#156)
    >- fa9f0c5dec5fc794257ddc0a032d21ccd0cf4e0c sroyal-statsig
    >   - Hash Secondary Exposures for GCIR (#155)

--- dotnet-statsig-tests/Common/StatsigTest.cs
+++ dotnet-statsig-tests/Common/StatsigTest.cs
@@ -21,7 +21,7 @@ namespace dotnet_statsig_tests
     {
         WireMockServer _server;
 
-        private const String ExpectedSdkVersion = "1.27.2.0";
+        private const String ExpectedSdkVersion = "1.27.3.0";
 
         Task IAsyncLifetime.InitializeAsync()
         {
