e89b261fab54729b7e6dcde25f44e0b7f5ee4137
sroyal-statsig <76536058+sroyal-statsig@users.noreply.github.com>
2024-11-25 15:49:48 -0800

    [release] 1.29.0 - Use CDN for DCS Requests (#167)
    ### Improvements
      - Switches to using a get request to get config specs from cdn
    
    
    
    
    
    >Included In This Release
    >- 91cd1dfc15150c5a74c3e5d5dd8259b5057a96cc sroyal-statsig
    >   - Switch DCS to use Get Request (#165)
    >- 85ff9c2dc31044b90d2d31d53e62d58e35bfdc83 sroyal-statsig
    >   - Use CDN for DCS (#164)

--- dotnet-statsig-tests/Common/StatsigTest.cs
+++ dotnet-statsig-tests/Common/StatsigTest.cs
@@ -21,7 +21,7 @@ namespace dotnet_statsig_tests
     {
         WireMockServer _server;
 
-        private const String ExpectedSdkVersion = "1.28.1.0";
+        private const String ExpectedSdkVersion = "1.29.0.0";
 
         Task IAsyncLifetime.InitializeAsync()
         {
