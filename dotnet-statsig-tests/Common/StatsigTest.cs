cfd631470b72bc73e92c9d9a3ffa9a41a33ac320
sroyal-statsig <76536058+sroyal-statsig@users.noreply.github.com>
2025-04-24 13:12:07 -0700

    [release] 2.2.0 - Add Custom HttpClient Option to StatsigOptions (#182)
    ### New Features
    - Adds an option to StatsigOptions that allows providing a custom
    HttpClient that will be used for networking
      ### Improvements
      - Logs exposures for the unsupported evaluation case
    
    
    
    
    
    >Included In This Release
    >- 25e6f7014789ea5ce7fb66a027702cf8d176fd77 sroyal-statsig
    >   - Add HttpClient to StatsigOptions (#181)
    >- 782d26108b30dd126ed3e20863d04a4069037398 sroyal-statsig
    >   - Log Exposures for Unsupported (#179)

--- dotnet-statsig-tests/Common/StatsigTest.cs
+++ dotnet-statsig-tests/Common/StatsigTest.cs
@@ -21,7 +21,7 @@ namespace dotnet_statsig_tests
     {
         WireMockServer _server;
 
-        private const String ExpectedSdkVersion = "2.1.0.0";
+        private const String ExpectedSdkVersion = "2.2.0.0";
 
         Task IAsyncLifetime.InitializeAsync()
         {
